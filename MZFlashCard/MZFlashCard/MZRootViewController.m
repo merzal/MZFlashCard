55a76caaab3aeac32bfb4b95170941cc239b7894
Zalan Mergl <merzal@gmail.com>
2018-06-16 17:54:26 +0200

    Redesign starting screen

---
+++ MZFlashCard/MZFlashCard/MZRootViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  MZFlashCard
+//
+//  Created by Zalan Mergl on 2018. 06. 15..
+//
+
+#import "MZRootViewController.h"
+
+@interface MZRootViewController ()
+
+@end
+
+@implementation MZRootViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)learnButtonPushed:(id)sender
+{
+    
+}
+
+- (IBAction)settingsButtonPushed:(id)sender
+{
+    
+}
+
+@end
